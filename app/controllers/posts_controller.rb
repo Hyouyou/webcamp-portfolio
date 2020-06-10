2b3481636195abee0e9362945a163bde45e109ac
Hyouyou <mums.kh16.29@icloud.com>
2020-06-10 09:43:55 +0000

    create-post-new-page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,36 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to user_path(current_user)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :explanation, :post_image_id, :condition, :is_burden)
   end
 end
