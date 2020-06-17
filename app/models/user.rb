43e8f4880773ea28ee21ba6cde918d84c337bb02
Hyouyou <mums.kh16.29@icloud.com>
2020-06-17 04:53:59 +0000

    add-delete-system/temporary

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
 
 	validates :name, presence: true
 	validates :email, presence: true
+
+	def soft_delete
+		update_attribute(:deleted_at, Time.current)
+	end
+
+	def active_for_authentication?
+		super && !deleted_at
+	end
+
+	def inactive_message
+		!deleted_at ? super : :deleted_account
+	end
 end
