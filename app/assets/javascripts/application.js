ee376124a056a8976d4e00e19a5b7897fdcddf9d
Hyouyou <mums.kh16.29@icloud.com>
2020-07-14 05:54:19 +0000

    fix-layout/new-re-form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,6 +35,18 @@ $(document).on('turbolinks:load', function(){
 		}
 	});
 
+	$('#input-image').change(function(){
+
+		var file = this.files[0];
+		//Fileオブジェクトの作成
+		var fileReader = new FileReader();
+		fileReader.readAsDataURL(file);
+		//読み込みが完了、srcを書き換え
+		fileReader.onloadend = function() {
+			$('#re-img').attr('src', this.result);
+		}
+	});
+
 
 	$('#comment-submit').attr('disabled','disabled');
 
@@ -48,9 +60,9 @@ $(document).on('turbolinks:load', function(){
 
 	$('#tab-contents .tab[id != "tab1"]').hide();
 
-	$('#tab-menu a').click(function(){
+	$('#tab-menu a').click(function(event){
 		$("#tab-contents .tab").hide();
-		$("tab-menu .active").removeClass("active");
+		$("#tab-menu .active").removeClass("active");
 		$(this).addClass("active");
 		$($(this).attr("href")).show();
 		event.preventDefault();
